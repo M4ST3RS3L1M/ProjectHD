d89e1c166cc99b80eea409ea84119a47140e30e0
Gordon <thenorsestoic@gmail.com>
2022-04-04 21:10:50 +0200

    Validering av userinput för reg

--- register.php
+++ register.php
@@ -1,61 +1,85 @@
-<?php
-
-include('nav.php');
-
-if (isset($_POST['username']) and isset($_POST['password'])) {
-    $name       = $mysqli->real_escape_string($_POST['username']);
-    $pwd        = $mysqli->real_escape_string($_POST['password']);
-    $firstname  = $mysqli->real_escape_string($_POST['fname']);
-    $lastname   = $mysqli->real_escape_string($_POST['lname']);
-    $DOB        = $mysqli->real_escape_string($_POST['DOB']);
-    $eMail      = $mysqli->real_escape_string($_POST['eMail']);
-    $sex        = $mysqli->real_escape_string($_POST['sex']);
-    
-    $query = <<<END
-    INSERT INTO HD_Users (username, password, fname, lname, DOB, eMail, sex)
-        VALUES ('{$_POST['username']}', '{$_POST['password']}', '{$_POST['fname']}', '{$_POST['lname']}', '{$_POST['DOB']}', '{$_POST['eMail']}', '{$_POST['sex']}')
-
-END;
-    
-    if ($mysqli-> query($query) !== TRUE) {
-        die("Could not query database" . $mysqli->errno . " : " . $mysqli->error);
-        header('Location:index.php');
-    }
-}
-
-echo $navigation;
-
-?>
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
-        
+
+        <?php
+        require_once('nav.php'); // Needed for db connection
+
+        // Check to see if user is already logged in
+        session_start();
+        if (isset($_SESSION['username']) && isset($_SESSION['userid'])) {
+            header("Location: ./index.php");
+        }
+
+        // Get data from form whne register button is clicked
+        if (isset($_POST['register_button'])) {
+            $name       = $mysqli->real_escape_string($_POST['username']);
+            $pwd        = $mysqli->real_escape_string($_POST['password']);
+            $repeat_pwd        = $mysqli->real_escape_string($_POST['repeat_password']);
+            $firstname  = $mysqli->real_escape_string($_POST['fname']);
+            $lastname   = $mysqli->real_escape_string($_POST['lname']);
+            $DOB        = $mysqli->real_escape_string($_POST['DOB']);
+            $eMail      = $mysqli->real_escape_string($_POST['eMail']);
+            $sex        = $mysqli->real_escape_string($_POST['sex']);
+            
+            // Verify entered data
+            if ($name != "" && $pwd != "" && $repeat_pwd != "" && $eMail != "") {
+                // Check that entered passwords match
+                if ($pwd === $ $repeat_pwd) {
+                    // Check that pwd meets min req
+                    if (strlen($pwd) >= 5 && strpbrk($pwd, "!#$.,:;()") != false) {
+                        // Check if username is taken
+                        $query = "SELECT * FROM HD_Users WHERE username='{$name}'";
+                        $result = $mysqli->query($query);
+                        if ($result = "") {
+                            // Encrypt password
+                            $pwd = md5($pwd);
+                            // Insert query to create the user
+                            $query = "INSERT INTO HD_Users (username, password, fname, lname, DOB, eMail, sex) VALUES ('{$_POST['username']}', '{$_POST['password']}', '{$_POST['fname']}', '{$_POST['lname']}', '{$_POST['DOB']}', '{$_POST['eMail']}', '{$_POST['sex']}')";
+                            $mysqli-> query($query);
+                            // Verify account creation
+                            $query = "SELECT * FROM HD_Users WHERE username='{$name}'";
+                            if ($mysqli->query($query) == TRUE) {
+                                $success = true;
+                            }
+                            else
+                                $error_msg = 'An error occurred and your account was not created.';
+                        }
+                        else
+                            $error_msg = 'The username <i>'.$name.'</i> is already taken. Please use another.';
+                    }
+                    else
+                        $error_msg = 'Password is not strong enough, make sure it meets the minimum requirements.';
+                }
+                else
+                    $error_msg = 'The passwords do not match.';
+            }
+            else
+                $error_msg = 'Please fill out all the required fields.';    
+        }
+        ?>
+
         <title>Register</title>
         <meta charset="utf-8">
 	</head>
 
 	<body>
 		<h1>Register an account</h1>
-		
-        <!--nav and shit-->
 
         <?php
-            if (isset($success) && $success == true) {
-                echo 'Your account has been created. <a href="login.php">Click here</a> to login';
-            }
-            //dubbelt???
+        echo $navigation;
         ?>
-        
+
         <form action="register.php" class="form" method="POST">
             <h1>Create account</h1>
 
             <div class="">
                 <?php
                     if (isset($success) && $success == true) {
-                        echo '<p>Your account has been created. <a href="./login.php">Click here</a> to login!</p>';
+                        echo '<p color="green">Your account has been created. <a href="./login.php">Click here</a> to login!</p>';
                     }
                     else if (isset($error_msg)) {
-                        echo '<p>'.$error_msg.'</p>';
+                        echo '<p color="red">'.$error_msg.'</p>';
                     }
                     else {
                         echo ''; // do nothing
@@ -76,19 +100,19 @@ echo $navigation;
                 <input type="password" name="repeat_password" placeholder="Repeat Password" autocomplete="off" required />
             </div>
             <div class="">
-                <input type="text" name="fname" placeholder="First Name" autocomplete="off" required />
+                <input type="text" name="fname" placeholder="First Name" autocomplete="off" />
             </div>
             <div class="">
-                <input type="text" name="lname" placeholder="Last Name" autocomplete="off" required />
+                <input type="text" name="lname" placeholder="Last Name" autocomplete="off" />
             </div>
             <div class="">
-                <input type="text" name="DOB" placeholder="Date of Birth" autocomplete="off" required />
+                <input type="text" name="DOB" placeholder="Date of Birth" autocomplete="off" />
             </div>
             <div class="">
                 <input type="text" name="eMail" placeholder="eMail" autocomplete="off" required />
             </div>
             <div class="">
-                <input type="text" name="sex" placeholder="Sex" autocomplete="off" required />
+                <input type="text" name="sex" placeholder="Sex" autocomplete="off" />
             </div>
 
             <div class="">
