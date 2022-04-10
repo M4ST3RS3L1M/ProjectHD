356395b3f03c34e0431b0fb3cf5f6a881364b8aa
M4ST3RS3L1M <96353867+M4ST3RS3L1M@users.noreply.github.com>
2022-04-10 23:56:50 +0200

    Drop down fixed

--- login.php
+++ login.php
@@ -50,25 +50,25 @@ END;
 
 <section class="vh-100">
     <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="row d-flex justify-content-center py-5 h-100">
             <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                 <div class="card bg-dark text-white" style="border-radius: 1rem;">
                     <div class="card-body p-5 text-center">
 
-                        <div class="mb-md-5 mt-md-4">
+                        <div class="mb-md-1 mt-md-1">
 
-                            <h2 class="fw-bold mb-2 text-uppercase">Login to start your journey!</h2>
-                            <p class="text-white-50 mb-5">Please enter your username and password!</p>
+                            <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                            <p class="text-white-50 mb-2">Please enter your username and password to start your journey!</p>
               
                             <form action="login.php" method="post" id="main">  
                                 <div class="form-outline form-white mb-4">
-                                    <label class="form-label" for="username">Username</label>
+                                    <label class="form-label text-white" for="username">Username</label>
                                     <input id="loginName" type="text" name="username" class="form-control form-control-lg">
                 
                                 </div>
 
                                 <div class="form-outline form-white mb-4">
-                                    <label class="form-label" for="typePasswordX">Password</label>
+                                    <label class="form-label text-white" for="typePasswordX">Password</label>
                                     <input id="typePasswordX" type="password" name="password" class="form-control form-control-lg">
                 
                                 </div>
